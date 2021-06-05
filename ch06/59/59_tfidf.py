e1ff5d33dc8c4371e7e5535cfc4e9e4fb203f4e2
Shinichiro Saito <s-saito@chic.ocn.ne.jp>
2021-06-05 12:55:13 +0900

    add 59

---
+++ ch06/59/59_tfidf.py
@@ -0,0 +1,43 @@
+# tf-idfによる精度確認
+import sys
+sys.path.append('../../src')
+
+import pickle
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from util import make_predict_by_LR
+
+train_y = pd.read_table('../50/train.txt', usecols=['CATEGORY'])
+valid_y = pd.read_table('../50/valid.txt', usecols=['CATEGORY'])
+test_y = pd.read_table('../50/test.txt', usecols=['CATEGORY'])
+
+train_feature = np.loadtxt('../51/train.feature.tfidf.txt')
+valid_feature = np.loadtxt('../51/valid.feature.tfidf.txt')
+test_feature = np.loadtxt('../51/test.feature.tfidf.txt')
+
+result_list = []
+
+for c in [0.1,0.5,1,10,100]:
+    result = make_predict_by_LR(
+        train_y,
+        valid_y,
+        test_y,
+        train_feature,
+        valid_feature, 
+        test_feature, 
+        C=c)
+
+    result_list.append(result)
+
+result_df = pd.DataFrame(result_list)
+result_df = result_df.set_index(0)
+result_df.columns = ['train','valid','test']
+result_df.plot()
+
+plt.title('tfidf LR')
+plt.xlabel('c')
+plt.ylabel('Accuracy')
+plt.savefig('result.png')
