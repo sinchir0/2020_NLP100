9d22e7f9b029797efe0a230c54e659762b93f084
root <s-saito@chic.ocn.ne.jp>
2021-06-14 22:53:43 +0000

    ch07

--- README.md
+++ README.md
@@ -14,3 +14,27 @@ https://github.com/taro-masuda/nlp100
 <br>
 takapy0210さん
 https://github.com/takapy0210/nlp_2020
+
+# docker環境の立ち上げ方
+
+```
+# buildとrun
+# -dはbackgroundでの起動
+docker-compose up --build -d 
+
+# runだけしたい場合
+docker-compose up -d
+
+# docker-composeで構築した環境の確認方法
+docker-compose ps
+
+# fishでのdocker containerへの接続
+# appの部分はdocker-compose.ymlで指定したservice名。存在しない名前だと、何も起きないため注意。
+docker-compose exec app fish 
+
+# network, container 削除
+docker-compose down
+
+# network, container, images 削除
+docker-compose down --rmi all
+```
---
+++ ch07/60/60_getfile.py
@@ -0,0 +1,43 @@
+# 60. 単語ベクトルの読み込みと表示
+# Google Newsデータセット（約1,000億単語）での学習済み単語ベクトル（300万単語・フレーズ，300次元）をダウンロードし，
+# ”United States”の単語ベクトルを表示せよ．ただし，”United States”は内部的には”United_States”と表現されていることに注意せよ．
+
+# 引用
+# https://www.mahirokazuko.com/entry/2019/04/27/134235
+
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    file_id = '0B7XkCwpI5KDYNlNUTTlSS21pQmM'
+    destination = 'GoogleNews-vectors-negative300.bin.gz'
+    
+    download_file_from_google_drive(file_id, destination)
