782ba67f259e0d3772ef34c8b34c966910017b57
Shinichiro Saito <s-saito@chic.ocn.ne.jp>
2021-06-28 19:07:05 +0900

    add ch02

---
+++ ch02/11/11.py
@@ -0,0 +1,14 @@
+# 11. タブをスペースに置換
+# タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
+
+if __name__ == "__main__":
+
+    with open("../10/popular-names.txt") as f:
+        popular_names = f.readlines()
+
+    popular_names = [text.replace('\t',' ') for text in popular_names]
+
+    print(''.join(popular_names))
+
+    # UNIXコマンド
+    # cat  ../10/popular-names.txt | tr '\t' ' '
---
+++ ch02/13/13.py
@@ -0,0 +1,16 @@
+# 13. col1.txtとcol2.txtをマージ
+# 12で作ったcol1.txtとcol2.txtを結合し，
+# 元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．
+# 確認にはpasteコマンドを用いよ．
+
+if __name__ == "__main__":
+
+    with open("../12/col1.txt") as f:
+        col1 = f.readlines()
+
+    with open("../12/col2.txt") as f:
+        col2 = f.readlines()
+
+    col1_col2_merge = [col1.replace('\n','') + '\t' + col2.replace('\n','') for col1, col2 in zip(col1,col2)]
+
+    print(col1_col2_merge)
